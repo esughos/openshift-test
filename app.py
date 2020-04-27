387fe7d5ba83e74007a61272250bf8f15a98ac0f
esughos <30291416+esughos@users.noreply.github.com>
2020-04-27 21:57:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello IBM !"
+    return "Hello it's my first Openshift Pipeline program with jenkins!!!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
